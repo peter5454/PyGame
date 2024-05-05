b1fed0b30993acc245433d1d6987d5389348b751
Albie-GH <lbng02@gmail.com>
2024-05-06 09:50:56 +1200

    Added enemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from enemy import Enemy
 import constants as c
 
 #initialise pygame
@@ -9,7 +10,16 @@ clock = pg.time.Clock()
 
 #create game window
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
-pg.display.set_caption("Tower Defence")
+pg.display.set_caption("Medieval Meltdown")
+
+#load images
+enemy_image = pg.image.load('assets/images/enemies/enemy_1.png').convert_alpha()
+
+#create groups
+enemy_group = pg.sprite.Group()
+
+enemy = Enemy((200, 300), enemy_image)
+enemy_group.add(enemy)
 
 #game loop
 run = True
@@ -17,10 +27,21 @@ while run:
 
   clock.tick(c.FPS)
 
+  screen.fill("grey100")
+
+  #update groups
+  enemy_group.update()
+
+  #draw groups
+  enemy_group.draw(screen)
+
   #event handler
   for event in pg.event.get():
     #quit program
     if event.type == pg.QUIT:
       run = False
+
+  #update display
+  pg.display.flip()
 
 pg.quit()
